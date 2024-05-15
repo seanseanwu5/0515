bfc0d69d7fd96658a4427b6eaadd7edee2a8f958
seanseanwu5 <seanseanwu5@gmail.com>
2024-05-15 17:00:44 +0800

    make it better

--- app/src/main/java/com/example/a0515/MainActivity.kt
+++ app/src/main/java/com/example/a0515/MainActivity.kt
@@ -3,12 +3,26 @@ package com.example.a0515
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.a0515.ui.theme._0515Theme
 
@@ -22,25 +36,91 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Tap()
                 }
             }
         }
     }
 }
 
+//@Composable
+//fun PointerEvents() {
+//    var msg by remember { mutableStateOf("") }
+//    Column {
+//        Text(msg)
+//        Box(
+//            Modifier
+//                .fillMaxSize()
+//                .background(Color.Yellow)
+//                .pointerInput(Unit) {
+//                    awaitPointerEventScope {
+//                        while (true) {
+//                            val event = awaitPointerEvent()
+//                            msg = "${event.type}, ${event.changes.first().position}"
+//                        }
+//                    }
+//                }
+//        )
+//    }
+//}
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Tap() {
+    var msg by remember { mutableStateOf("TAP相關手勢實例") }
+    var offset1 by remember { mutableStateOf(Offset.Zero) }
+    var offset2 by remember { mutableStateOf(Offset.Zero) }
+    var PU = arrayListOf(R.drawable.pu0, R.drawable.pu1,
+        R.drawable.pu2, R.drawable.pu3,
+        R.drawable.pu4, R.drawable.pu5)
+    var Number by remember { mutableStateOf(0) }
+
+
+    Column {
+        Text(text = msg)
+
+        Image(
+            painter = painterResource(id =  PU[Number]),
+            contentDescription = "靜宜之美",
+            modifier = Modifier
+                .fillMaxSize()
+//                .pointerInput(Unit) {
+//                    detectTapGestures(
+//                        onTap = {msg = "後觸發onTap(短按)"},
+//                        onDoubleTap = {msg = "雙擊"},
+//                        onLongPress = {msg = "長按"},
+//                        onPress = {msg = "先觸發onPress(按下)"}
+//                    )
+//                }
+                .pointerInput(Unit) {
+                    detectDragGesturesAfterLongPress(
+                        onDrag = { change, dragAmount -> offset2+=dragAmount},
+                        onDragStart = {
+                            offset1 = it
+                            offset2 = it },
+                        onDragEnd = {
+                            if (offset2.x >= offset1.x){
+                                msg = "長按後向右拖曳"
+                                Number ++
+                                if (Number>5){Number=0}
+                            }
+                            else{
+                                msg = "長按後向左拖曳"
+                                Number --
+                                if (Number<0){Number=5}
+                            }
+
+                        },
+                    )
+                }
+
+
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun PointerEventsPreview() {
     _0515Theme {
-        Greeting("Android")
+        Tap()
     }
 }
